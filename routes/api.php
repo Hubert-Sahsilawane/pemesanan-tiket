ae9d1618e5327fe5dc5e03b698eca453c8f8fc39
Hubert-Sahsilawane <susisusilowati4334@gmail.com>
2025-07-21 15:54:51 +0700

    menambahkan scramble dan membernarkan error dari register, login, dan logout

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RegisterController;
-use App\Http\Controllers\Api\LoginController;
-use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
-Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
-Route::middleware('auth:api')->post('/logout', [LogoutController::class, 'logout']);
+Route::post('auth/register', [RegisterController::class, 'register']);
+Route::post('auth/login', [LoginController::class, 'login']);
+Route::post('auth/logout', [LogoutController::class, 'logout']);
